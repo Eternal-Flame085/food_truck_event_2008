f738af6752525253df4be9ac435167a2a9fcc26c
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-15 15:27:21 -0400

    Created stock and its test

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,6 +1,19 @@
 class FoodTruck
-  attr_reader :name
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory.has_key?(item)
+      @inventory[item]
+    else
+      0
+    end
+  end
+
+  def stock(item, stock)
+    @inventory[item] = stock
   end
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -13,6 +13,25 @@ class FoodTruckTest < Minitest::Test
   def test_it_has_readable_variables
     food_truck = FoodTruck.new("Rocky Mountain Pies")
 
+    expected = {}
     assert_equal "Rocky Mountain Pies", food_truck.name
+    assert_equal expected, food_truck.inventory
+  end
+
+  def test_check_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    assert_equal 0, food_truck.check_stock(item1)
+  end
+
+  def test_food_truck_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    food_truck.stock(item1, 30)
+
+    expected = {item1 => 30}
+    assert_equal expected, food_truck.inventory
   end
 end
