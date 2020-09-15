77a8fdedb8cb8f9955d7967baa6fe79da1d8bffa
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-15 15:35:17 -0400

    added more to stock test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -34,10 +34,15 @@ class FoodTruckTest < Minitest::Test
   def test_food_truck_stock
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     food_truck.stock(item1, 30)
 
-    expected = {item1 => 30}
-    assert_equal expected, food_truck.inventory
+    expected1 = {item1 => 30}
+    expected2 = {item1 => 55, item2 => 12}
+    assert_equal expected1, food_truck.inventory
+
+    food_truck.stock(item1, 25)
+    food_truck.stock(item2, 12)
+    assert_equal expected2, food_truck.inventory
   end
 end
